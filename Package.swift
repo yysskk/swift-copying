59776d96020f31f6eaa630f438aadd0dc343f4a6
Yusuke Morishita <hello@yusuke.ca>
2026-08-12 14:59:59 -0700

    chore: declare the SwiftSyntaxBuilder dependency explicitly

--- Package.swift
+++ Package.swift
@@ -33,6 +33,7 @@ let package = Package(
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
                 .product(name: "SwiftDiagnostics", package: "swift-syntax"),
                 .product(name: "SwiftBasicFormat", package: "swift-syntax"),
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"),
             ]
         ),
         .target(name: "Copying", dependencies: ["CopyingMacros"]),
